dcd3925294a8553ef3859f5b7cce455f6218059d
asherginting <asherginting3@gmail.com>
2024-08-09 11:32:00 +0700

    feat: add configure project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Albert Sans', 'sans-serif'], 
+        signature: ['TheSignature', 'cursive'], 
+        unbounded: ['Unbounded', 'sans-serif'],
+      },
+      colors: {
+        gold: '#D6B66B',
+        cream: '#FAF9F5',
+        teal: '#0B7373',
+        darkTeal: '#004040',
+        black: '#000000',
+        gray: '#B8B8B8',
+      },
+      backgroundColor: {
+        gold: '#D6B66B',
+        black: '#000000',
+        teal: '#0B7373',
+        darkTeal: '#004040',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
